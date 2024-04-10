03fafe0a80e360054b47e7e34231769c8b5fdc73
Robert Tipter <roberttipter@Roberts-MacBook-Air.local>
2024-04-10 19:08:11 +0300

    Fixed link

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import requests, re
 
 class Booker():
     def __init__(self) -> None:
-        # self.url = """https://bigfitness.upfit.live/class-
-        #             schedule.php?clubid=1001&type=1&template=3&lang=ro&notify=true"""
-        # self.response = requests.get(self.url).text
-        with open('./output.txt', 'r') as f:
-            self.response = f.read()
+        self.url = """https://bigfitness.upfit.live/class-schedule.php?clubid=1001&type=1&template=3&lang=ro&notify=true"""
+        self.response = requests.get(self.url).text
+        # with open('./output.txt', 'r') as f:
+        #     self.response = f.read()
         self.classes = {}
         self.get_class_id(self.response)
         self.get_class_names()
@@ -26,8 +25,7 @@ class Booker():
         pattern = r'id="upfcl-class-name-(\d+)"'
         for match in re.finditer(pattern, response):
             match = match.group(1)
-            self.classes[match]={}
-            
+            self.classes[match]={}            
         return self.classes
     
     def get_class_names(self):
@@ -41,6 +39,9 @@ class Booker():
             _pattern = r'<div class="upfcl-class-date" id="upfcl-class-date-{0}" style="display:none">(\d+\.\d+\.\d+)</div>'.format(class_id)
             for line in re.finditer(_pattern, self.response):
                 self.classes[class_id]["date"] = line.group(1)
+                
+        # print(self.response)
+        print(self.classes)
                 
 Booker()
         
